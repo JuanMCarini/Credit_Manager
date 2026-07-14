28114ea47ad9b476519e9c1e2a6479f4194cd06b
Juan <cartera@fideisainversion.com.ar>
2026-07-14 15:35:51 -0300

    chore: remove obsolete database migration scripts and temporary error logs

--- migrate.py
+++
@@ -1,7 +0,0 @@
-from src.database.connection import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    conn.execute(text("ALTER TABLE tasas_y_comisiones ADD COLUMN porcentaje_sellado NUMERIC(15, 6) DEFAULT 0.0;"))
-    conn.commit()
-print("DB Updated!")
