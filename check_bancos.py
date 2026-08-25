f2f52c6ac787100de22788dd847f0df34e3029c5
Juan <cartera@fideisainversion.com.ar>
2026-08-25 10:20:42 -0300

    refactor: remove obsolete check_bancos diagnostic script

--- check_bancos.py
+++
@@ -1,8 +0,0 @@
-from src.database import SessionLocal
-from src.database.models.finance.bancos import Banco
-
-session = SessionLocal()
-bancos = session.query(Banco).all()
-for b in bancos:
-    print(f"Banco: {b.nombre}, ID: {b.id}, Parser Type: {b.parser_type}")
-session.close()
